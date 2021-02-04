aaf2a1ba2e65090308703dd424f2f06bb941cc3c
samira-estes-garcia <samira.estes.garcia@gmail.com>
2021-02-04 11:29:05 -0600

    added more tests for mycomponent, created file to add custom hook

--- src/MyComponent.test.js
+++ src/MyComponent.test.js
@@ -11,10 +11,31 @@ describe('MyComponent', () => {
     let wrapper;
     beforeEach(() => {
         wrapper = mount(<MyComponent onCountChange={onCountChange} />)
+        //count is being set to 0 every time
     });
     it('renders', () => {
-        console.log(wrapper.debug());
+        // console.log(wrapper.debug());
         expect(wrapper).not.toBeNull();
+    })
+    //when call button, make sure useState button is correctly updated
+    it('shows my default text', () => {
+        expect(wrapper.find('p').text()).toEqual('Count: 0');
+    })
+    it('shows it correctly increments the count by 1', () => {
+        wrapper.find('button').simulate('click');
+        expect(wrapper.find('p').text()).toEqual('Count: 1');
+    })
+    it('shows it correctly increments the count by 4', () => {
+        wrapper.find('button').simulate('click');
+        wrapper.find('button').simulate('click');
+        wrapper.find('button').simulate('click');
+        wrapper.find('button').simulate('click');
+        expect(wrapper.find('p').text()).toEqual('Count: 4');
+    })
+    it('shows it correctly increments the count by 1', () => {
+        expect(onCountChange).toBeCalledTimes(1);
+        wrapper.find('button').simulate('click');
+        expect(onCountChange).toBeCalledTimes(2);
     })
 })
 
